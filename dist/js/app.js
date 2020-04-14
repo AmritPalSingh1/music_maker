ce22acbf1c300fc0d9a9856c4e6f0f81c3d11c16
asingh369 <amritpalsinghs369@gmail.com>
2020-04-14 14:34:25 -0700

    added pause play

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,131 +1,143 @@
 const options_container = document.querySelector(".options-container");
 const playhead = document.querySelector(".playhead");
+const pause_btn = document.getElementById("pause_btn");
 
-options_container.addEventListener('click', alter_options);
+options_container.addEventListener("click", alter_options);
+pause_btn.addEventListener("click", pause_play);
 
 let current_translate = 0;
 
+let selected = [
+  [0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1],
+];
 
-let selected = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]];
-
-function alter_options(e){
-  
+function alter_options(e) {
   let row = 0;
   let column = 0;
 
-  if (e.target.nodeName == "I"){
-    row = parseInt(e.target.parentElement.getAttribute('row'));
-    column = parseInt(e.target.parentElement.getAttribute('column'));
+  if (e.target.nodeName == "I") {
+    row = parseInt(e.target.parentElement.getAttribute("row"));
+    column = parseInt(e.target.parentElement.getAttribute("column"));
     add_icon(e.target, row, column);
-  }
-  else{
-    row = e.target.getAttribute('row');
-    column = e.target.getAttribute('column');
-    add_icon(e.target.children[0], row, column);
+  } else {
+    row = e.target.getAttribute("row");
+    column = e.target.getAttribute("column");
+    if (row != null) {
+      add_icon(e.target.children[0], row, column);
+    }
   }
 }
 
-function add_icon(element, row, column){
-  if (!element.classList.contains('fa-circle')){
+function add_icon(element, row, column) {
+  if (!element.classList.contains("fa-circle")) {
     element.className = "far fa-circle";
-    selected[row-1][column-1] = 0;
-  }
-  else if (row == 1){
+    selected[row - 1][column - 1] = 0;
+  } else if (row == 1) {
     element.className = "fas fa-star fix-size light-text-1";
-    selected[row-1][column-1] = 1;
-  }
-  else if (row == 2){
+    selected[row - 1][column - 1] = 1;
+  } else if (row == 2) {
     element.className = "fas fa-cube fix-size light-text-2";
-    selected[row-1][column-1] = 1;
-  }
-  else if (row == 3){
+    selected[row - 1][column - 1] = 1;
+  } else if (row == 3) {
     element.className = "fab fa-sketch fix-size light-text-3";
-    selected[row-1][column-1] = 1;
-  }
-  else{
+    selected[row - 1][column - 1] = 1;
+  } else {
     element.className = "fas fa-square fix-size light-text-4";
-    selected[row-1][column-1] = 1;
+    selected[row - 1][column - 1] = 1;
   }
 }
 
-function transform(){
+function transform() {
   const col_width = get_col_width();
 
-  if (screen.width > 576){
+  if (screen.width > 576) {
     current_translate = current_translate + 1;
-  }
-  else{
+  } else {
     current_translate = current_translate + 0.75;
   }
 
-  if (current_translate > col_width*6){
+  if (current_translate > col_width * 6) {
     current_translate = 0;
   }
 
   playhead.style.transform = `translate3d(${current_translate}px, 0px, 0px)`;
 
   // Handle play music cases
-  if ((current_translate + col_width/2) % col_width == 0 && current_translate != 0){
-    const column_hit = (current_translate + col_width/2) / col_width;
+  if (
+    (current_translate + col_width / 2) % col_width == 0 &&
+    current_translate != 0
+  ) {
+    const column_hit = (current_translate + col_width / 2) / col_width;
     // Play sound
-    if (selected[0][column_hit-1] == 1){
+    if (selected[0][column_hit - 1] == 1) {
       playSound("a");
       addAnimation(1, column_hit);
     }
-    if (selected[1][column_hit-1] == 1){
+    if (selected[1][column_hit - 1] == 1) {
       playSound("b");
       addAnimation(2, column_hit);
     }
-    if (selected[2][column_hit-1] == 1){
+    if (selected[2][column_hit - 1] == 1) {
       playSound("c");
       addAnimation(3, column_hit);
     }
-    if (selected[3][column_hit-1] == 1){
+    if (selected[3][column_hit - 1] == 1) {
       playSound("d");
       addAnimation(4, column_hit);
     }
-
   }
-
-
 }
 
+var interval = setInterval(transform, 1);
 
-setInterval(transform , 1);
-
-function get_col_width(){
+function get_col_width() {
   return document.querySelector(".sample-width").offsetWidth;
 }
 
-
-function playSound(sound){
+function playSound(sound) {
   const synth = new Tone.Synth().toMaster();
 
-  if (sound == "a"){
+  if (sound == "a") {
     //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease('D2', '8n');
-  }
-  else if (sound == "b"){
+    synth.triggerAttackRelease("D2", "8n");
+  } else if (sound == "b") {
     synth.triggerAttackRelease("B3", "8n");
-  }
-  else if (sound == "c"){
+  } else if (sound == "c") {
     synth.triggerAttackRelease("A3", "8n");
-  }
-  else if (sound == "d"){
+  } else if (sound == "d") {
     synth.triggerAttackRelease("C3", "8n");
   }
 }
 
-function addAnimation(box_id, column_hit){
+function addAnimation(box_id, column_hit) {
   // Animate first box
   const box = document.getElementById(`box${box_id}`);
-  box.classList.add('active');
+  box.classList.add("active");
 
-  setTimeout(function(){ box.classList.remove('active'); }, 250);
+  setTimeout(function () {
+    box.classList.remove("active");
+  }, 250);
 
   const btn = document.getElementById(`${box_id}${column_hit}`).children[0];
-  btn.classList.add('active');
+  btn.classList.add("active");
 
-  setTimeout(function(){ btn.classList.remove('active'); }, 250);
+  setTimeout(function () {
+    btn.classList.remove("active");
+  }, 250);
+}
 
+function pause_play(e) {
+  $(".text-pause").toggleClass("fa-pause-circle");
+  $(".text-pause").toggleClass("fa-play-circle");
+
+  if (e.target.classList.contains("fa-pause-circle")) {
+    // Play
+    interval = setInterval(transform, 1);
+  } else {
+    // Pause
+    clearInterval(interval);
+  }
 }
